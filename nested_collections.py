cc64407f8f3c67c8ce114e4de030f8b42777c639
DoodyShark <da2863@nyu.edu>
2024-04-25 19:08:20 -0400

    Basic setup

---
+++ nested_collections.py
@@ -0,0 +1,75 @@
+"""
+nested_collections implementation for mongodb database management and organization
+"""
+
+from typing import AnyStr
+from pymongo import MongoClient, collection
+
+
+class NestedCollection:
+    """
+    class implementation of NestedCollection
+    """
+
+    def __init__(self, name: AnyStr, root_db: MongoClient) -> None:
+        """
+        initializer
+        """
+        self.root_db = root_db
+        self.name = name
+        self.config = self.root_db.nested_collections.find_one({"name": name})
+        self.dict = {}
+        for col in self.config["children"]:
+            # collection[0] : pseudonym - collection[1] : collection name
+            self.dict[col[0]] = col[1]
+
+    def name_from_pseudonym(self, pseudonym: AnyStr) -> AnyStr:
+        """
+        get mapping between pseudonym and name
+        """
+        return self.dict[pseudonym]
+
+    def list_collections(self) -> None:
+        """
+        list all collection
+        """
+        print(self.config["children"])
+
+    def add_collection(self, pseudonym: AnyStr, name: AnyStr) -> None:
+        """
+        add a collection
+        """
+        self.config["children"].append((pseudonym, name))
+        self.dict[pseudonym] = name
+        self.root_db.nested_collections.update_one(
+            {"name": self.name}, {"$set": {"children": self.config["children"]}}
+        )
+
+    def remove_collection(self, pseudonym: AnyStr) -> None:
+        """
+        remove a collection
+        """
+        for i in range(len(self.config["children"])):
+            if self.config["children"][i][0] == pseudonym:
+                del self.dict[pseudonym]
+                self.config["children"].pop(i)
+                self.root_db.nested_collections.update_one(
+                    {"name": self.name}, {"$set": {"children": self.config["children"]}}
+                )
+                return
+
+    def __getitem__(self, pseudonym) -> collection:
+        """
+        get a collection by psuedonym
+        """
+        try:
+            return self.root_db.get_collection(self.dict[pseudonym])
+        except KeyError:
+            print("Collection", pseudonym, "doesn't exist...")
+            return None
+
+    def __contains__(self, pseudonym):
+        """
+        check if collection by pseydonym exists
+        """
+        return pseudonym in self.dict
---
+++ tests/test_flask_webapp.py
@@ -0,0 +1,82 @@
+"""
+    Tests for functions used in the webapp
+"""
+
+import pytest
+from app import create_app, end_mgd
+
+
+class Tests:
+    """
+    Class for handling tests
+    """
+
+    @pytest.fixture
+    def app(self):
+        """
+        Creates an app
+        """
+
+        app = create_app()
+
+        app.config.update(
+            {
+                "TESTING": True,
+            }
+        )
+
+        assert app.connected
+
+        # other setup can go here
+
+        yield app
+
+        # clean up / reset resources here
+        if not hasattr(app, "db"):
+            app.db = None
+        if not hasattr(app, "se5_db"):
+            app.se5_db = None
+        end_mgd(app.db, app.se5_db)
+
+    def shut_pylint(self, smthn):
+        """A test to shut pylint up"""
+        print("This test should shut it up", smthn)
+
+    def shut_pylint1(self, smthn):
+        """A test to shut pylint up"""
+        print("This test should shut it up", smthn)
+
+    def shut_pylint2(self, smthn):
+        """A test to shut pylint up"""
+        print("This test should shut it up", smthn)
+
+
+# def test_api(self, app):
+#     """
+#     tests whether api works at creating app
+#     """
+
+#     headers = {"Content-Type": "multipart/form-data"}
+
+#     buffer = None
+
+#     with open("test.raw", "rb") as f:
+#         buffer = io.BytesIO(f.read())
+
+#     data = {}
+
+#     data["audio"] = (buffer, "audio")
+
+#     # Create a test client using the Flask application configured for testing
+#     with app.test_client() as test_client:
+#         response = test_client.post("/api/transcribe", data=data, headers=headers)
+
+#         assert response.status_code == 200
+
+#         # Check if the response content type is JSON
+#         assert response.content_type == "application/json"
+#         # Check if the response contains the expected key
+#         data = json.loads(response.data.decode("utf-8"))
+#         assert "transcription" in data
+
+#         assert "down with containers" in data.values()
