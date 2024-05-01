8bccdd2103a739251c304eca65724e818d286e71
DoodyShark <da2863@nyu.edu>
2024-05-01 10:05:32 -0400

    Reverting

--- src/fizzbuzz_draw/app.py
+++ src/fizzbuzz_draw/app.py
@@ -15,7 +15,7 @@ from src.fizzbuzz_draw.setup_mg import end_mgd, start_mgd
 config = dotenv_values(".env")
 
 ## create a socketio object
-socketio = SocketIO(ping_timeout=60*10, ping_interval=60)
+socketio = SocketIO()
 
 ROOM_SIZE = 3
 
